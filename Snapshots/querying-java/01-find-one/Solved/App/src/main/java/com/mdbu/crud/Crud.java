9406f1e5975777a87238b872395673efbf22321f
xijing-zhang <xijingzh@gmail.com>
2022-10-03 18:07:16 -0400

    Adds Snapshots for Find One Lab

---
+++ Snapshots/querying-java/01-find-one/Solved/App/src/main/java/com/mdbu/crud/Crud.java
@@ -0,0 +1,28 @@
+package com.mdbu.crud;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.Document;
+import org.bson.BsonValue;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Crud {
+    private final MongoCollection<Document> collection;
+
+    public Crud(MongoClient client) {
+        this.collection = client.getDatabase("bank").getCollection("accounts");
+    }
+
+    public void insertOneDocument(Document doc) {
+        System.out.println("Inserting one account document");  
+        InsertOneResult result = collection.insertOne(doc);    
+        BsonValue id = result.getInsertedId();  
+        System.out.println("Inserted document Id: " + id);
+    }
+}
