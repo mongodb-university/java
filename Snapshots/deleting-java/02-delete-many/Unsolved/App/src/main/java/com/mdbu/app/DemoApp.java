c470627fe49d0e4ab175e82ffd95f032f156f7aa
xijing-zhang <xijingzh@gmail.com>
2022-10-04 13:06:00 -0400

    Adds snapshots for delete many and restructures delete one

---
+++ Snapshots/deleting-java/02-delete-many/Unsolved/App/src/main/java/com/mdbu/app/DemoApp.java
@@ -0,0 +1,44 @@
+package com.mdbu.app;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import com.mdbu.aggregations.Aggregation;
+import com.mdbu.crud.Crud;
+import com.mdbu.transactions.Transaction;
+import com.mdbu.utils.MongoClientSingleton;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static com.mongodb.client.model.Aggregates.match;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Sorts.descending;
+import static com.mongodb.client.model.Sorts.orderBy;
+import static java.util.Arrays.asList;
+
+
+public class DemoApp {
+    public static void main(final String[] args) {
+        Logger root = (Logger) LoggerFactory.getLogger("org.mongodb.driver");
+        // Available levels are: OFF, ERROR, WARN, INFO, DEBUG, TRACE, ALL
+        root.setLevel(Level.WARN);
+
+        try (MongoClient client = MongoClientSingleton.getClient()) {
+            //CRUD
+            Crud crud = new Crud(client);
+            //DELETE MANY
+            Bson documentsToDelete = eq("balance", 1500);
+            crud.deleteManyDocuments(documentsToDelete);
+        }
+    }
+}
