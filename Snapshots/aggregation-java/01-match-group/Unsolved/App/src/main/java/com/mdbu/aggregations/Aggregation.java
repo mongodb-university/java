1b45417caed7f40e7fdc85548af28c9ab1bb1eda
xijing-zhang <xijingzh@gmail.com>
2022-10-05 17:29:47 -0400

    Adds Snapshots for Match Group lab

---
+++ Snapshots/aggregation-java/01-match-group/Unsolved/App/src/main/java/com/mdbu/aggregations/Aggregation.java
@@ -0,0 +1,35 @@
+package com.mdbu.aggregations;
+
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Aggregates;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import com.mdbu.aggregations.Aggregation;
+
+import static com.mongodb.client.model.Sorts.orderBy;
+import static java.util.Arrays.asList;
+import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Sorts.descending;
+
+import java.util.List;
+
+public class Aggregation {
+    private final MongoCollection<Document> collection;
+
+    public Aggregation(MongoClient client) {
+        this.collection = client.getDatabase("banking").getCollection("accounts");
+    }
+
+    public void showAccountTypeSummary() {
+        Bson matchStage = null; //TODO: define the match stage
+        Bson groupStage = null; //TODO: define the group stage
+        List<Bson> pipeline = List.of(matchStage, groupStage);
+        AggregateIterable<Document> result = collection.aggregate(pipeline);
+        result.forEach(document -> System.out.println(document.toJson()));
+    }
+    
+}
+
